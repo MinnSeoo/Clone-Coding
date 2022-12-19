aaad09bdb17404b9866fb0e9a8a1bb8a78dbf3e4
정민서 <102645965+MinnSeoo@users.noreply.github.com>
2022-12-19 12:53:16 +0900

    파일 위치변경 및 코드수정 + js 파일,코드 추가

---
+++ 1117_정민서/JS/funtion.js
@@ -0,0 +1,25 @@
+const item = document.querySelector(".box1");
+
+item.addEventListener("click", () => {
+    alert("멍멍!!");
+})
+
+
+const item2 = document.querySelector(".color-2");
+
+item2.addEventListener("click", () => {
+    alert("아우웅~");
+})
+
+
+const item3 = document.querySelector(".color-3");
+
+item3.addEventListener("click", () => {
+    alert("꼬꼬ㄷㅔㄱ~");
+})
+
+const item5 = document.querySelector(".color-5");
+
+item5.addEventListener("click", () => {
+    alert("새미새미~");
+})
